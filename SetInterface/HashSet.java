195b6e4af032ae0336e9489c90478f7bea8039a2
Min Thu Kha <minthukha@Mins-MacBook-Air.local>
2024-08-14 23:52:12 +0900

    last commit

--- SetInterface/HashSet.java
+++
@@ -1,5 +0,0 @@
-package SetInterface;
-
-public class HashSet {
-    
-}
---
+++ SetInterface/HashSetClass.java
@@ -0,0 +1,44 @@
+package SetInterface;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+public class HashSetClass {
+    public static void main(String[] args) {
+        HashSet<Integer> evenNumber = new HashSet<>();
+
+        // Using add() method
+        evenNumber.add(2);
+        evenNumber.add(4);
+        evenNumber.add(6);
+        System.out.println("HashSet: " + evenNumber);
+
+        HashSet<Integer> numbers = new HashSet<>();
+        
+        // Using addAll() method
+        numbers.addAll(evenNumber);
+        numbers.add(5);
+        System.out.println("New HashSet: " + numbers);
+
+        // Calling iterator() method
+        Iterator<Integer> iterate = numbers.iterator();
+        System.out.print("HashSet using Iterator: ");
+        // Accessing elements
+        while(iterate.hasNext()) {
+            System.out.print(iterate.next());
+            System.out.print(", ");
+        }
+
+
+        // Using remove() method
+        boolean value1 = numbers.remove(5);
+        System.out.println("Is 5 removed? " + value1);
+
+        boolean value2 = numbers.removeAll(numbers);
+        System.out.println("Are all elements removed? " + value2);
+
+    }
+   
+    
+}
